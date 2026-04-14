4758a7c078be9b89657640834a99a3bc08e6ecd4
EduArita-coder <eduardoj1109@gmail.com>
2026-04-14 09:38:35 -0600

    Se modifico algunos parametros para una mejor comprension del codigo

--- StreetStatusAPI/Entities/Location.cs
+++ StreetStatusAPI/Entities/Location.cs
@@ -1,16 +1,23 @@
-#nullable enable
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace StreetStatusAPI.Entities
 {
+    [Table("locations")]
     public class Location
     {
+        [Required]
+        [StringLength(32)]
         public int Id { get; set; }
-        public string City { get; set; } = string.Empty;
-        public string Province { get; set; } = string.Empty;
-        public string Country { get; set; } = string.Empty;
-        public string ZipCode { get; set; } = string.Empty;
+        [StringLength(40)]
+        public string City { get; set; }
+        [StringLength(40)]
+        public string Province { get; set; }
+        [StringLength(20)]
+        public string Country { get; set; } 
+        [StringLength(10)]
+        public string ZipCode { get; set; }
         public DateTime CreatedDate { get; set; }
 
-        // Relación con Streets
-        public ICollection<Street> Streets { get; set; } = new List<Street>();
     }
 }
